fafadbe260653e794591775b77f4354dbedda1c1
Bayu <bayu@Bayus-MacBook-Air.local>
2016-11-20 13:30:14 +0700

    Add filter JNE trucking service

--- app/code/local/Ansyori/Aongkir/Model/System/Config/Source/Serpis.php
+++ app/code/local/Ansyori/Aongkir/Model/System/Config/Source/Serpis.php
@@ -3,7 +3,7 @@
  * Indonesia Shipping Carriers
  * @copyright   Copyright (c) 2015 Ansyori B.
  * @email		ansyori@gmail.com / ansyori@kemanaservices.com
- * @build_date  March 2015   
+ * @build_date  March 2015
  * @license     http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  */
 class Ansyori_Aongkir_Model_System_Config_Source_Serpis
@@ -14,13 +14,13 @@ class Ansyori_Aongkir_Model_System_Config_Source_Serpis
      *
      * @return array
      */
-	
+
 	public function helper()
 	{
 		return  Mage::helper('aongkir');
 	}
-	
-	
+
+
 	public function getSql()
 	{
 		$sql2 = "
@@ -31,7 +31,7 @@ class Ansyori_Aongkir_Model_System_Config_Source_Serpis
 		  `service_text` varchar(255) DEFAULT NULL,
 		  PRIMARY KEY (`idx`)
 		) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-		
+
 		INSERT IGNORE INTO `aongkir_service_list` (`idx`, `kurir`, `servis`, `service_text`) VALUES
 		(1,	'JNE',	'CTC',	'JNE (CTC) JNE City Courier'),
 		(2,	'JNE',	'CTCOKE',	'JNE (CTCOKE) JNE City Courier'),
@@ -41,17 +41,21 @@ class Ansyori_Aongkir_Model_System_Config_Source_Serpis
 		(6,	'JNE',	'REG',	'JNE (REG) Layanan Reguler'),
 		(7,	'JNE',	'SPS',	'JNE (SPS) Super Speed'),
 		(8,	'JNE',	'YES',	'JNE (YES) Yakin Esok Sampai'),
-		(9,	'POS',	'Express Next Day',	'POS (Express Next Day) Express Next Day'),
-		(10,	'POS',	'Surat Kilat Khusus',	'POS (Surat Kilat Khusus) Surat Kilat Khusus'),
-		(11,	'TIKI',	'ECO',	'TIKI (ECO) Economi Service'),
-		(12,	'TIKI',	'HDS',	'TIKI (HDS) Holiday Delivery Service'),
-		(13,	'TIKI',	'ONS',	'TIKI (ONS) Over Night Service'),
-		(14,	'TIKI',	'REG',	'TIKI (REG) Regular Service'),
-		(15,	'TIKI',	'SDS',	'TIKI (SDS) Same Day Service'),
-		(16,	'TIKI',	'TDS',	'TIKI (TDS) Two Day Service');
-		
+    (9,	'JNE',	'JTR',	'JNE (JTR) JNE Trucking'),
+    (10,	'JNE',	'JTR<150',	'JNE (JTR<150) JNE Trucking'),
+    (11,	'JNE',	'JTR250',	'JNE (JTR250) JNE Trucking'),
+    (12,	'JNE',	'JTR>250',	'JNE (JTR>250) JNE Trucking'),
+		(13,	'POS',	'Express Next Day',	'POS (Express Next Day) Express Next Day'),
+		(14,	'POS',	'Surat Kilat Khusus',	'POS (Surat Kilat Khusus) Surat Kilat Khusus'),
+		(15,	'TIKI',	'ECO',	'TIKI (ECO) Economi Service'),
+		(16,	'TIKI',	'HDS',	'TIKI (HDS) Holiday Delivery Service'),
+		(17,	'TIKI',	'ONS',	'TIKI (ONS) Over Night Service'),
+		(18,	'TIKI',	'REG',	'TIKI (REG) Regular Service'),
+		(19,	'TIKI',	'SDS',	'TIKI (SDS) Same Day Service'),
+		(20,	'TIKI',	'TDS',	'TIKI (TDS) Two Day Service');
+
 		";
-		
+
 		try{
 			$resource = Mage::getSingleton('core/resource');
 			$writeConnection = $resource->getConnection('core_write');
@@ -63,7 +67,7 @@ class Ansyori_Aongkir_Model_System_Config_Source_Serpis
 			return false;
 		}
 	}
-	
+
 	public function toOptionArray()
 	{
 		$this->getSql();
@@ -73,59 +77,59 @@ class Ansyori_Aongkir_Model_System_Config_Source_Serpis
 		$resource = Mage::getSingleton('core/resource');
 		$writeConnection = $resource->getConnection('core_read');
 		$data =   $writeConnection->fetchAll($sql);
-		
+
 		foreach($data as $serpis)
 		{
 			$arrs_serpis[] =  array(
-			
-					'value' => $serpis['kurir'].'|'.$serpis['servis'], 
-					'label'=> $serpis['service_text'] 
+
+					'value' => $serpis['kurir'].'|'.$serpis['servis'],
+					'label'=> $serpis['service_text']
 					);
 		}
-		
+
 		return $arrs_serpis;
-		
+
 	}
-	
+
     public function xtoOptionArray()
     {
-		
-		
+
+
 		$this->getSql();
-		
+
 		if($this->helper()->getApiKey()):
-		
+
 		//$this->helper()->saveAreaToDb();
-		
+
 			$city_array = array();
 			$city_list = Mage::getModel('aongkir/area')->getCollection()->setOrder('province','ASC');
-			
+
 			$count = Mage::getModel('aongkir/area')->getCollection()->count();
-			
+
 			if(!$count)
 			{
 				$this->helper()->saveAreaToDb();
 			}
-			
+
 			$city_array[] = array('value' => '', 'label'=> '--- Select Origin ('.$count.' Cities Registered) ---' );
 			foreach($city_list->getData() as $data)
 			{
 				$city_id =  $data['city_id'];
 				$city_name = "Provinsi ".$data["province"]." ".$data["type"]." ".$data["city_name"]." Kodepos ".$data["postal_code"];
 				$city_array[] = array('value' => $city_id, 'label'=> $city_name );
-				
+
 			}
-		 
+
 		 	//asort($city_array,);
 			//uasort($city_array, 'cmp');
 			return $city_array;
-		 
-		
+
+
 		else:
 			return array(
-			
+
 				array('value' => 1, 'label'=>Mage::helper('adminhtml')->__('API KEY INVALID')),
-				
+
 			);
 		endif;
     }
