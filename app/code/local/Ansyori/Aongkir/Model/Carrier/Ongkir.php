2597c08f8269d657797ab82aafee001e406c8c36
bayu_rrahmadi <bayu.r.rahmadi@gmail.com>
2017-02-21 15:07:28 +0700

    remove marketplace method

--- app/code/local/Ansyori/Aongkir/Model/Carrier/Ongkir.php
+++ app/code/local/Ansyori/Aongkir/Model/Carrier/Ongkir.php
@@ -22,19 +22,19 @@ class Ansyori_Aongkir_Model_Carrier_Ongkir
 			$count = 0;
 			foreach($list_euy as $jangar_kana_hulu)
 			{
+				if ($jangar_kana_hulu['cost'] === 0) {
+					$jangar_kana_hulu['cost'] = 9000;
+				}
+
 				$count++;
+				//getMethodRates($code,$title,$name,$rates)
 				$method = $this->getMethodRates('_'.$count,'',$jangar_kana_hulu['text'],$jangar_kana_hulu['cost']);
 				$result->append($method);
 			}
 
-			// NYANG BAROE
-			// foreach ($list_euy as $key => $_rates) {
-			// 	foreach ($_rates as $_rate) {
-			// 		$count++;
-			// 		$_option = $this->getMethodRates('_'.$count,$key,$_rate['text'],$_rate['cost']);
-			// 		$result->append($_option);
-			// 	}
-			// }
+
+
+
             return $result;
         }
 
@@ -56,51 +56,39 @@ class Ansyori_Aongkir_Model_Carrier_Ongkir
 
 		}
 
-		public function getOriginsId()
-		{
-			return $this->helper()->config('origin');
-		}
-
 		public function getListRates()
 		{
-			$marketplace_item = $this->getItems();
+			$origin = $this->getOriginId();
 			$dest = $this->getCityId();
-			// $weight = $this->getBeratTotal() + 0;
-			$product_count = count($marketplace_item);
+			$weight = $this->getBeratTotal();
 
 			$carriers = $this->getActiveCarriers();
+
 			$rate_list = array();
-			foreach ($carriers as $kurir) {
-				foreach ($marketplace_item as $seller_item) {
-					$origin = $seller_item['city'];
-					// Mage::log('each origin: '.Zend_Debug::dump($origin, null, false), null, 'bayu.log');
-					$weight = $seller_item['weight'] + 0;
+			//getRates($origin,$dest,$weight,$kurir)
+			foreach($carriers as $kurir)
+			{
+				if($weight > 29){
+					$rates_by_kurir = $this->helper()->getSavedRates($origin,$dest,1,$kurir);
+				}else{
 					$rates_by_kurir = $this->helper()->getSavedRates($origin,$dest,$weight,$kurir);
-					// Mage::log('each call: '.Zend_Debug::dump($rates_by_kurir, null, false), null, 'bayu.log');
-					foreach ($rates_by_kurir as $final_list) {
-						$rate_list_2[$final_list['text']][] = array(
-							'cost' 	 => $final_list['cost'],
-							'weight' => $weight,
-						);
-					}
-				}
-			}
-			Mage::log('rates per-item: '.Zend_Debug::dump($rate_list_2, null, false), null, 'bayu.log');
-			foreach ($rate_list_2 as $key => $rates) {
-				$total_weight = 0;
-				$total_cost = 0;
-				foreach ($rates as $rate) {
-					$total_weight += $rate['weight'];
-					$total_cost += $rate['cost'];
-					$ratex = array(
-						'text' => $key . "(" . $total_weight . "Kg)",
-						'cost' => $total_cost,
+				};
+				foreach($rates_by_kurir as $final_list)
+				{
+					if($weight > 29):
+						$ship_cost = $this->changePrice($final_list['cost']) * $weight;
+					else:
+						$ship_cost = $this->changePrice($final_list['cost']);
+					endif;
+
+					$rate_list[] = array(
+						'text' => $final_list['text'] . "($weight Kg)",
+						'cost' => $ship_cost
+
 					);
 				}
-				if (count($rates) == $product_count)
-					array_push($rate_list, $ratex);
 			}
-			Mage::log('total rates: '.Zend_Debug::dump($rate_list, null, false), null, 'bayu.log');
+			$this->helper()->setLog('Final rate '.print_r($rate_list,true));
 			return $rate_list;
 
 		}
@@ -185,83 +173,9 @@ class Ansyori_Aongkir_Model_Carrier_Ongkir
 			endif;
 		}
 
-		public function getItems()
+		public function getOriginId()
 		{
-			$products = array();
-			$_items = Mage::getSingleton('checkout/session')->getQuote()->getAllItems();
-			foreach ($_items as $_item)
-			{
-				$product_seller	= Mage::getModel('marketplace/product')->getCollection()
-					->addFieldToFilter('mageproductid',$_item->getProduct()->getId());
-				if ($product_seller)
-				{
-					foreach ($product_seller as $product) {
-
-						// Mage::log('iki kotane: '.Zend_Debug::dump($_item->getProduct()->getName(), null, false), null, 'bayu.log');
-						$item_address = Mage::getModel('customer/address')->load($product[product_address_id]);
-						$city_text = '';
-						if ($item_address) $city_text = $item_address->getCity();
-
-						// Mage::log('iki kotane: '.Zend_Debug::dump($city_text, null, false), null, 'bayu.log');
-
-						$sql = "select * from ".Mage::getConfig()->getTablePrefix()."daftar_alamat where concat(type,' ',city_name) = '$city_text' limit 0,1 ";
-						$res = $this->helper()->fetchSql($sql);
-						// Mage::log('Ada gak kotanya: '.Zend_Debug::dump($res, null, false), null, 'bayu.log');
-						$city_id = $res[0]['city_id'];
-
-						$products[] = array(
-							'weight'  => $_item->getProduct()->getWeight(),
-							'city' => $city_id,
-						);
-
-					}
-				}
-			}
-			// Mage::log($products);
-			// Mage::log('products: '.Zend_Debug::dump($products, null, false), null, 'bayu.log');
-			return $products;
-		}
-
-		public function getOriginId_perSeller()
-		{
-			$product_ids = array();
-	    $seller_ids = array();
-	    $seller_list = array();
-			$ship_addresses = array();
-
-			$_items = Mage::getSingleton('checkout/session')->getQuote()->getAllItems();
-			foreach ($_items as $_item) {
-				$product_seller	= Mage::getModel('marketplace/product')->getCollection()
-					->addFieldToFilter('mageproductid',$_item->getProduct()->getId());
-
-				if ($product_seller) {
-					foreach ($product_seller as $sellervalue) {
-						$seller_id = $sellervalue->getUserid();
-						$seller_id = (string) $seller_id;
-						if($seller_id)
-						{
-							array_push($seller_ids, $seller_id);
-							if (!isset($seller_list[$seller_id]['products'])) {
-								$seller_list[$seller_id]['products'] = array();
-							}
-							array_push($seller_list[$seller_id]['products'], $_item->getProduct()->getWeight());
-						}
-					}
-				}
-			}
-
-			foreach ($seller_ids as $seller_id) {
-				$ship_address = Mage::getModel('customer/customer')->load($seller_id)
-					->getPrimaryBillingAddress();
-				if ($ship_address) {
-					$city_text = $ship_address->getCity();
-					$sql = "select * from ".Mage::getConfig()->getTablePrefix()."daftar_alamat where concat(type,' ',city_name) = '$city_text' limit 0,1 ";
-					$res = $this->helper()->fetchSql($sql);
-					$seller_list[$seller_id]['city'] = $res[0]['city_id'];
-				}
-			}
-			// Mage::log($seller_list);
-			return $seller_list;
+			return $this->helper()->config('origin');
 		}
 
 		public function getDisabledServices()
